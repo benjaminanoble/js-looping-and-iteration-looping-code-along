981d6f4aef9429040520946e39fa9f49c22a4b7a
benjaminnoble <benjaminanoble@icloud.com>
2021-07-19 22:32:14 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, surprise) {
+    let cards = []
+    for ( let i = 0; i < names.length; i++ ) {
+      cards.push( `Thank you, ${names[i]}, for the wonderful ${surprise} gift!` )
+    }
+    return cards
+  }
+
+  const firstNumber = 10
+
+function countDown(firstNumber) {
+    while(firstNumber > 0) {
+        console.log(firstNumber);
+        firstNumber -=1
+    }
+    console.log(firstNumber);
+}
